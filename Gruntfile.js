535a099635f437f0d767acd5397167787f510128
Frederic Heem <frederic.heem@gmail.com>
2015-02-02 21:53:12 +0000

    v2 WIP

---
+++ Gruntfile.js
@@ -0,0 +1,285 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Configurable paths
+    var config = {
+        app: 'app',
+        dist: 'dist'
+    };
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Project settings
+        config: config,
+        shell: {
+            browserify: {
+                options: {
+                    stdout: true,
+                    stderr: true
+                },
+                command: './node_modules/browserify/bin/cmd.js lib/StateDiagramEditor.js --standalone <%= pkg.name %> -o browser/dist/<%= pkg.name %>.standalone.js'
+            }
+        },
+        browserify: {
+            options: {
+                detectGlobals: false,
+                transform: ['browserify-ejs']
+            },
+
+            development: {
+                options: {
+                    debug: true
+                },
+                files: {
+                    '<%= config.dist %>/app.js': '<%= config.app %>/main.js'
+                }
+            }
+        },
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            bower: {
+                files: ['bower.json'],
+                tasks: ['bowerInstall']
+            },
+            js: {
+                files: ['<%= config.app %>/{,*/}*.{js,html}', 'lib/*.js'],
+                tasks: ['shell','browserify:development','jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            jstest: {
+                files: ['test/spec/{,*/}*.js'],
+                tasks: ['test:watch']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            styles: {
+                files: ['<%= config.app %>/styles/{,*/}*.css'],
+                tasks: ['concat:development_css'],
+                options: {
+                    livereload: true
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/{,*/}*.html',
+                    '<%= config.app %>/images/{,*/}*'
+                ]
+            }
+        },
+
+        concat: {
+            development_css: {
+                options: {
+                    separator: '\n'
+                },
+
+                files: {
+                    '<%= config.dist %>/styles/main.css': 
+                        [
+                         '<%= config.app %>/styles/main.css'
+                         ]
+                }
+            }
+        },
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                open: true,
+                livereload: 35730,
+                // Change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            connect().use('/bower_components', connect.static('./bower_components')),
+                            connect().use('/vendor', connect.static('./vendor')),
+                            connect.static(config.dist),
+                            connect.static('browser/dist')
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    open: false,
+                    port: 9001,
+                    middleware: function(connect) {
+                        return [
+                            connect.static('test'),
+                            connect().use('/bower_components', connect.static('./bower_components')),
+                            connect().use('/vendor', connect.static('./vendor')),
+                            connect.static(config.dist),
+                            connect.static('browser/dist')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    base: '<%= config.dist %>',
+                    livereload: false
+                }
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '<%= config.dist %>/*',
+                        '!<%= config.dist %>/.git*'
+                    ]
+                }]
+            }
+        },
+
+        // Make sure code styles are up to par and there are no obvious mistakes
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= config.app %>/scripts/{,*/}*.js',
+                '!<%= config.app %>/scripts/vendor/*',
+                'test/spec/{,*/}*.js'
+            ]
+        },
+
+        // Mocha testing framework configuration options
+        mocha: {
+            all: {
+                options: {
+                    run: true,
+                    urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
+                }
+            }
+        },
+
+        // Automatically inject Bower components into the HTML file
+        bowerInstall: {
+            app: {
+                src: ['<%= config.app %>/index.html'],
+                exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']
+            }
+        },
+        ejs: {
+            options: {
+                environment: process.env.NODE_ENV || 'dev'
+            },
+
+            'index.html': {
+                src: '<%= config.app %>/index.html',
+                dest: '<%= config.dist %>/index.html'
+            }
+        },
+        // Copies remaining files to places other tasks can use
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= config.app %>',
+                    dest: '<%= config.dist %>',
+                    src: [
+                        'img/*.{ico,png}',
+                        '{,*/}*.html',
+                        'styles/fonts/{,*/}*.*'
+                    ]
+                }, {
+                    expand: true,
+                    dot: true,
+                    cwd: 'bower_components/bootstrap/dist',
+                    src: ['fonts/*.*'],
+                    dest: '<%= config.dist %>'
+                }]
+            },
+            styles: {
+                expand: true,
+                dot: true,
+                cwd: '<%= config.app %>/styles',
+                dest: '.tmp/styles/',
+                src: '{,*/}*.css'
+            }
+        },
+
+        // Run some tasks in parallel to speed up build process
+        concurrent: {
+            server: [
+                'copy:styles'
+            ],
+            test: [
+                'copy:styles'
+            ],
+            dist: [
+                'copy:styles'
+            ]
+        }
+    });
+
+    grunt.registerTask('serve', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'ejs',
+            'concat',
+            'shell:browserify',
+            'browserify:development',
+            'copy:dist',
+            'concurrent:server',
+            'connect:livereload',
+            'watch'
+            
+        ]);
+    });
+
+    grunt.registerTask('test', function (target) {
+        if (target !== 'watch') {
+            grunt.task.run([
+                'concurrent:test'
+            ]);
+        }
+
+        grunt.task.run([
+            'connect:test',
+            'mocha'
+        ]);
+    });
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'concat',
+        'shell:browserify',
+        'browserify:development',
+        'concurrent:dist',
+        'copy:dist'
+    ]);
+
+    grunt.registerTask('default', [
+        'newer:jshint',
+        'test',
+        'build'
+    ]);
+};
---
+++ app/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var StateMachineController = require('../lib/StateMachineController.js');
+
+
+var App = function(){
+    var stateMachineController = new StateMachineController()
+};
+
+var app = new App();
+//app.start();
+
